617c0d25d6a36c4294ac0b82c004b80bbf98ada4
jvy0001@alu.medac.es <jvy0001@alu.medac.es>
2025-12-02 19:24:43 +0100

    API_0.1
    Propiedades y base de datos añadida con los requisitos pedidos

---
+++ Api_BDS/Api_BDS/src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS ALUMNO;
+
+CREATE TABLE Cliente (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(25),
+    apellidos VARCHAR(25),
+    dni VARCHAR(9),
+    CaducidadDni DATE,
+    FechaNacimiento DATE,
+    Telefono INT(9),
+    Nacionalidad  VARCHAR(30),
+    seguro CHAR(1),
+    correo VARCHAR(20)
+);
+
